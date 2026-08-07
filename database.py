a1f511d900715a32815542fa098d01de73ea057b
kotlatejasree <kotlatejasree@gmail.com>
2026-08-07 22:37:44 +0530

    Change database connection URL for Aiven
    Updated database connection URL to use Aiven cloud service.

--- database.py
+++ database.py
@@ -1,6 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker,declarative_base
-Database_URL="mysql+pymysql://root:Tejasree%402006@localhost:3306/student_db"
+Database_URL="mysql://avnadmin:AVNS_36e8KzEiDkODfxHUC-V@tejasree2006-dbfastapicrud.f.aivencloud.com:19288/defaultdb?ssl-mode=REQUIRED"
+#Database_URL="mysql+pymysql://root:Tejasree%402006@localhost:3306/student_db"
 engine=create_engine(Database_URL)
 sessionlocal=sessionmaker(
     autocommit=False,
