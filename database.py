aedf13161675afe8fc9f91bd27517c6701b1b94d
kotlatejasree <kotlatejasaree@gmail.com>
2026-08-08 10:46:20 +0530

    database aiven

--- database.py
+++ database.py
@@ -1,6 +1,6 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker,declarative_base
-Database_URL="mysql+pymysql://avnadmin:AVNS_36e8KzEiDkODfxHUC-V@tejasree2006-dbfastapicrud.f.aivencloud.com:19288/defaultdb?ssl-mode=REQUIRED"
+Database_URL="mysql+pymysql://avnadmin:AVNS_36e8KzEiDkODfxHUC-V@tejasree2006-dbfastapicrud.f.aivencloud.com:19288/defaultdb?"
 engine=create_engine(Database_URL)
 sessionlocal=sessionmaker(
     autocommit=False,
